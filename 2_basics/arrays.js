3d968762139ccb47bdd95bab11773cefb006b71e
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-24 07:06:05 +0000

    array

---
+++ 2_basics/arrays.js
@@ -0,0 +1,25 @@
+//ARRAYS PART1
+
+// const arr=[1,2,3,4,5,6,7,8]
+// console.log(arr);
+
+// arr.push(11)
+// console.log(arr);
+// arr.push(192)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// arr.unshift(1111)
+// console.log(arr);
+
+
+// const a=[1,2,3,4,5,6,7,8,9]
+// console.log("a",a);
+
+// console.log(a.slice(1,3))
+// console.log("b",a);
+
+// console.log(a.splice(1,3))
+// console.log("c",a);
