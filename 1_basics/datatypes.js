55df3db79b012524e1d448874aaaa52d015fa91d
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-24 01:11:49 +0000

    daatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,15 @@
+let id=100
+console.log(id);
+console.log(typeof id);
+
+let name="rushikesh"
+console.log(name);
+console.log(typeof name );
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+let fail=true
+console.table([id,name,fail])
+
+
