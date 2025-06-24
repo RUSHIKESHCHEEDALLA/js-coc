3d9122cb804f2125518b97c8d304282bc4f2122e
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-24 01:19:51 +0000

    conversions

---
+++ 1_basics/typeconversions.js
@@ -0,0 +1,18 @@
+let id=100
+let sid=String(id)
+// console.log(id);
+// console.log(typeof id);
+
+// console.log(sid);
+// console.log(typeof sid);
+
+let val=undefined;
+let nullval=Boolean(val)
+
+// console.log(nullval)
+
+let num=true;
+console.log(num)
+let nums=String(num)
+console.log(nums)
+
