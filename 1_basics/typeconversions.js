1ac7b8adcc044e2744f2200760e7299234545f73
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-24 01:43:12 +0000

    operations

--- 1_basics/typeconversions.js
+++ 1_basics/typeconversions.js
@@ -12,7 +12,24 @@ let nullval=Boolean(val)
 // console.log(nullval)
 
 let num=true;
-console.log(num)
+// console.log(num)
 let nums=String(num)
-console.log(nums)
+// console.log(nums)
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let a= 1 + 2 + 2
+let b= 1+2 + 1 +"0"
+let c= "1"+2 + 2
+let d= "1"+"2" +"2"
+// console.table([a,b,c,d])
+// console.table([typeof a ,typeof b,typeof c, typeof d])
+
+let x=3
+let y=x++
+console.table([x,y])
