7122fcdbfb80da9329eb849787a54d8b4cf83f67
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-24 07:31:00 +0000

    a2

--- 2_basics/arrays.js
+++ 2_basics/arrays.js
@@ -23,3 +23,22 @@
 
 // console.log(a.splice(1,3))
 // console.log("c",a);
+
+
+
+// console.log(Array.isArray("rushi"))
+// console.log(Array.from("rushi"))
+// console.log(Array.from({name:"rushi"}))//intresting
+
+// let s1=10
+// let s2=100
+// console.log(Array.of(s1,s2))
+
+
+// const a=["hi","bye","hello"]
+// const b=["nye","byeee"]
+
+// console.log("A",a)
+// console.log("B",b)
+// const c=[...a,...b]
+// console.log("C",c)
