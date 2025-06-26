578f5c6f30fb1d6dea4e2aced145d6ca033a6051
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-26 00:45:24 +0000

    whlee

---
+++ 4_basics/for.js
@@ -0,0 +1,8 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     if (i==2) {
+//         continue
+//     }
+//     if ( i==5) break;   
+// }
+
---
+++ 4_basics/while.js
@@ -0,0 +1,26 @@
+let i=10
+while (i<=100) {
+    // console.log(i);
+    i=i+10;
+}
+
+let j=1
+while (j<=10) {
+    // console.log(j);
+    j=j+1;
+}
+
+let k=0
+const arr= ["hello","rushi","kesh"]
+
+while (k<arr.length) {
+    // console.log(arr[k]);
+    k++;
+}
+
+
+let c=1
+do {
+    console.log(c);
+    c++
+} while (c<5);
