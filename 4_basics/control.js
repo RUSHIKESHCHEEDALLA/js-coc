3102c27a32f1818004c4eaf6510fc21a8d68d0a1
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-06-26 00:15:49 +0000

    truth

---
+++ 4_basics/control.js
@@ -0,0 +1,48 @@
+//control flow
+
+const val=100
+if (val<=100){
+    // console.log("temperature is less than 100 ");
+}
+//nullish coalesing operator ??
+let val1;
+val1=5 ?? 10
+// console.log(val1);
+
+val1=null ?? 10
+// console.log(val1);
+
+val1= 10??null
+// console.log(val1);
+
+// terinary operator
+const val2=10
+val2>=80 ? console.log("val2 >= 80 ") : console.log("val2 < 80");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
